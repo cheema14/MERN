5d0489e0015d99e785b26fc14967a979b2715e39
ali hassan <ali.hassan@pitb.gov.pk>
2022-02-25 16:14:09 +0500

    chapter 3 completed. Last episode connection with mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 const app = require('./app');
+const connectDatabase = require('./config/database')
 
 const dotenv = require('dotenv');
 
 // Setting up config
-
 dotenv.config({ path: 'backend/config/config.env' })
+
+
+// Connecting to DB
+connectDatabase();
 
 app.listen(process.env.PORT, () => {
     console.log(`Server started on port ${process.env.PORT} in ${process.env.NODE_ENV} mode.`);
